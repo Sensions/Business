c176b94348b6c562c4f30337d8aa6dcbac34545c
Sension <svyatikkhmil@gmail.com>
2021-04-28 14:00:04 +0300

    add new1

--- mobile.js
+++ mobile.js
@@ -1,13 +1,18 @@
 (function(){
-    const burgerItem = document.querySelector('.header-burger');
-    const menu = document.querySelector('.header-links-bg-fixed-mobile');
-let menuCloseItem = document.querySelector(".header-nav-close-mobile");
+     burgerItem = document.querySelector('.header-burger');
+    let menu = document.querySelector('.header-links-bg-fixed-mobile');
+    let menuCloseItem = document.querySelector(".header-nav-close-mobile");
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header-links-bg-fixed-mobile-active');
-    });
+        document.body.classList.add('overflow')
+    })
     menuCloseItem.addEventListener('click', () =>{
         menu.classList.remove('header-links-bg-fixed-mobile-active');
+        document.body.classList.remove('overflow')
     })
+
 }())
+
+
 
  
